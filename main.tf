64fdd2c98c9920c79615ae15690ad023349bc2ee
dani.aguirre <daniel.aguirre@dinocloudconsulting.com>
2024-07-25 16:06:02 -0300

    test send logs

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ module "send_logs_archive" {
   source = "./modules/send-logs-archive"
   #   bucket_operation_name = "backups-logs-operation2"
   #   name_role_lambda      = "send_logs_to_archive-role"
-  #   account_id            = "767398058472"
+  ##   account_id            = "767398058472"
   bucket_archive_name = "backups-logs-archive2"
 }
